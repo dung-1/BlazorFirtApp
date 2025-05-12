1dc2a0a5636bc4908be62402c2a6beb2ccb9b82a
Dung Paulus CTS <linhtam2001a@gmail.com>
2025-05-12 22:28:44 +0700

    add okr

--- Services/NewsService.cs
+++ Services/NewsService.cs
@@ -16,5 +16,26 @@ namespace BlazorApp1.Services
             var result = await _client.From<News>().Get();
             return result.Models;
         }
+
+        public async Task AddNewsAsync(News news)
+        {
+            var response = await _client.From<News>().Insert(news);
+            if (response.Models == null || response.Models.Count == 0)
+            {
+                throw new Exception("Failed to add news to Supabase.");
+            }
+        }
+
+
+        public async Task UpdateNewsAsync(News news)
+        {
+            await _client.From<News>().Update(news);
+        }
+
+        public async Task DeleteNewsAsync(Guid id)
+        {
+            await _client.From<News>().Where(x => x.Id == id).Delete();
+        }
+
     }
 }
